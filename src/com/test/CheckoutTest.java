f3c9d0dbae94b8a5b12402a2cba81e42dcc66c91
yaowang0 <wyaocn@foxmail.com>
2018-01-02 15:49:39 +0000

    Hello

--- src/com/test/CheckoutTest.java
+++ src/com/test/CheckoutTest.java
@@ -1,13 +1,72 @@
 package com.test;
 
+import java.util.Scanner;
+
 /**
  * Created by YaoWang on 2018/1/2.
  *
  * Checkout System
  */
 public class CheckoutTest {
+    private static double totalPay = 0;
+    private static double applePrice = 0.6;
+    private static double orangePrice = 0.25;
 
     public static void main(String[] args) {
 
+        System.out.println("Please input your items:");
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        String[] strings = string.split(",");
+
+//        getPay(strings);
+
+        getOffers(strings);
+
+    }
+
+    /**
+     * Step1: Shopping cart
+     *
+     * @param strings
+     */
+    private static void getPay(String[] strings) {
+
+        for (String s : strings) {
+            if (s.trim().equalsIgnoreCase("apple")) {
+                totalPay += applePrice;
+            } else if (s.trim().equalsIgnoreCase("orange")) {
+                totalPay += orangePrice;
+            }
+        }
+        System.out.println("The total is: " + totalPay);
+    }
+
+    /**
+     * Step2: Simple offers
+     *
+     * @param strings
+     */
+    private static void getOffers(String[] strings) {
+
+        int appleNum = 0;
+        int orangeNum = 0;
+
+        for (String s : strings) {
+            if (s.trim().equalsIgnoreCase("apple")) {
+                appleNum++;
+                if (appleNum % 2 != 0) { // buy one and get one free
+                    totalPay += applePrice;
+                }
+            } else if (s.trim().equalsIgnoreCase("orange")) {
+                orangeNum++;
+                if (orangeNum % 3 != 0) { // new offers
+                    totalPay += orangePrice;
+                } else {
+                    orangeNum = 0;
+                }
+            }
+        }
+        System.out.println("The total is: " + totalPay);
     }
 }
